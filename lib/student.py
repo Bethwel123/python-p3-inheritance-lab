d92050cd65413979f45172d350e33b14ae44c664
Bethwel123 <bethwellangat654@gmail.com>
2024-09-03 17:01:05 +0300

    Practice code with inheritance

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, knowledge_str):
+        '''Add a string to the student's knowledge list.'''
+        self.knowledge.append(knowledge_str)
